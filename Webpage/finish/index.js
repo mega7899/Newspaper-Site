d458f54bf10e81b01324cd1a801e73766961e662
mega7899 <enchantingsworf@yahoo.com>
2025-05-26 18:01:50 +0800

    Add files via upload

---
+++ Webpage/finish/index.js
@@ -0,0 +1,6 @@
+/** @format */
+
+const dismissedBtn = (document.getElementById("submit_Btn").onclick = () => {
+  window.location.href =
+    "https://newspaper-site-56wqaj4uh-mega7899s-projects.vercel.app/";
+});
